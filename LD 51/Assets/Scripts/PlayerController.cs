f57dfb946162bba21dac2ead8570bad5416306b1
henrywangzh <henrywang3@g.ucla.edu>
2022-09-30 19:54:47 -0700

    basic player controller

---
+++ LD 51/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 2;
+    [SerializeField] float jumpHeight = 2;
+    Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkMovement();
+  
+    }
+
+    void checkMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float horizontalSpeed = x * speed;
+        body.velocity = new Vector2(horizontalSpeed, body.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        float jumpVelocity = Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight * body.gravityScale);
+        body.velocity = new Vector2(body.velocity.x, jumpVelocity);
+    }
+}
