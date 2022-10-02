0a89977b2fd835b52de96723ae8733f5ba1f415b
henrywangzh <henrywang3@g.ucla.edu>
2022-10-02 14:16:14 -0700

    added greyscale with post processing

---
+++ LD 51/Assets/GreyScreenScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GreyScreenScript : MonoBehaviour
+{
+
+    PostProcessVolume ppv;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ppv = GetComponent<PostProcessVolume>();
+        ppv.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerController.self.hp <= 0)
+        {
+            ppv.enabled = true;
+            Time.timeScale = 0.2f;
+        }
+    }
+}
