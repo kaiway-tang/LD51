0091fa8f99f8fa45748a490defeeee5aa0c40d3f
henrywangzh <henrywang3@g.ucla.edu>
2022-10-02 18:30:39 -0700

    adding powerup

---
+++ LD 51/Assets/Powerup.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+
+    Vector2[] spawnLocations;
+    Vector2 spawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void setPoints(Vector2[] points)
+    {
+        spawnLocations = points;
+        spawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
+        transform.position = spawnLocation;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 7 && collision.GetComponent<HPEntity>().ID == 1)
+        {
+            // this is the player
+            manager.self.applyPowerup();
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
