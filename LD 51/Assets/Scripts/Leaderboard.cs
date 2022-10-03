f74972421595c4f5370715ba05d501d2e8ca2b6c
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-10-02 20:34:25 -0700

    online leaderboard
    f-in finally this was so unnecesarily painful

---
+++ LD 51/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class Leaderboard : MonoBehaviour
+{
+    [SerializeField] InputField input1;
+    [SerializeField] InputField input0;
+    [SerializeField] TMP_Text playerDisp;
+    [SerializeField] TMP_Text scoreDisp;
+    
+
+    [SerializeField] string baseUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdi3X-YFdFZAK4PQ7PKycQ7RJq9EMBpyG9k8lZ4t0bze9lyTA/formResponse";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Sends post request to given url
+    IEnumerator Post(string scoreCount, string playerName)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.1826190743", playerName);
+        form.AddField("entry.1165984352", scoreCount); 
+        byte[] data = form.data;
+        WWW www = new WWW(baseUrl, data);
+        yield return www;
+    }
+
+    public void Submit()
+    {
+        StartCoroutine(Post(input1.text, input0.text));
+    }
+
+    public void Retrieve()
+    {
+        Debug.Log("Attempting call");
+        StartCoroutine(GetRequest());
+    }
+
+    IEnumerator GetRequest()
+    {
+        Debug.Log("Getting");
+        using (UnityWebRequest req = UnityWebRequest.Get("docs.google.com/spreadsheets/d/1XLYpqbOVAczZpyll9rSo69-HG7KwTBhyjc5dtYIhMHw/export?format=csv"))
+        {
+            Debug.Log("In progress");
+            yield return req.SendWebRequest();
+            string players = "";
+            string scores = "";
+            Debug.Log(req.downloadHandler.text);
+            foreach(string line in req.downloadHandler.text.Split('\n'))
+            {
+                string[] entry = line.Split(',');
+                // elem 0 is time, 1 is name, 2 is score
+                string name = entry[1];
+                string score = entry[2];
+                players += name + '\n';
+                scores += score + '\n';
+            }
+            playerDisp.text = players;
+            scoreDisp.text = scores;
+            
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
