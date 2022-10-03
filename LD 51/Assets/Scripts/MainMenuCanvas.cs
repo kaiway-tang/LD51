b028c80047d9e6283956ea03c07d6d610ee31ae2
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-10-03 14:23:33 -0700

    Tutorial

--- LD 51/Assets/Scripts/MainMenuCanvas.cs
+++ LD 51/Assets/Scripts/MainMenuCanvas.cs
@@ -8,6 +8,8 @@ public class MainMenuCanvas : MonoBehaviour
     [SerializeField] GameObject menu;
     [SerializeField] GameObject credits;
     [SerializeField] GameObject tutorial;
+
+    Anim knifeAnim;
     
     public enum Window
     {
@@ -22,6 +24,7 @@ public class MainMenuCanvas : MonoBehaviour
         menu.SetActive(true);
         credits.SetActive(false);
         tutorial.SetActive(false);
+        knifeAnim = GetComponent<Anim>();
     }
 
     public void ToggleDisplay(Window display)
@@ -29,6 +32,7 @@ public class MainMenuCanvas : MonoBehaviour
         menu.SetActive(display == Window.menu);
         credits.SetActive(display == Window.credits);
         tutorial.SetActive(display == Window.tutorial);
+        knifeAnim.HideKnives();
     }
 
     public void ToggleDisplay(int disp)
@@ -37,6 +41,7 @@ public class MainMenuCanvas : MonoBehaviour
         menu.SetActive(display == Window.menu);
         credits.SetActive(display == Window.credits);
         tutorial.SetActive(display == Window.tutorial);
+        knifeAnim.HideKnives();
     }
 
     public void StartGame()
